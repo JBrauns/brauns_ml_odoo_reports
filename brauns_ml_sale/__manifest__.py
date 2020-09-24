cd082c79b181536c34c82995478ddda55051052b
JBrauns <joshua_brauns@live.de>
2020-09-24 20:35:37 +0200

    Initial commit

---
+++ brauns_ml_sale/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Brauns-ML Sale",
+    "summray": "Allows to modify the default sales report template(s)",
+    "verion": "13.0.1.0.0",
+    "category": "Sales",
+    "author": "Joshua Brauns",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale"],
+    "data": [
+        "reports/sale_report.xml"
+    ]
+}
