405fb3f5bb54b8912f15a9a8366a4ae2bb600b46
JBrauns <joshua_brauns@live.de>
2020-10-04 21:18:27 +0200

    Dev update #10

--- brauns_ml_sale/models/ir_actions_report.py
+++ brauns_ml_sale/models/ir_actions_report.py
@@ -133,7 +133,7 @@ class IrActionsReport(models.Model):
 
         # Remove unnecessary layout elements
         #command_args = self.pop_command_arg(command_args, 'header-line', False)
-        command_args = self.pop_command_arg(command_args, 'header-spacing', True)
+        command_args = self.change_command_arg(command_args, 'header-spacing', '32')
 
         # disable smart shrinking to allow absolute positioning and size
         # necessary for a clean din 5008 document
