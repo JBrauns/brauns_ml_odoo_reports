931c6a14dfaf7edd9fa03e0267857ab224c9ee27
JBrauns <joshua_brauns@live.de>
2020-10-04 18:53:07 +0200

    Enable info logging for html header

--- brauns_ml_sale/models/ir_actions_report.py
+++ brauns_ml_sale/models/ir_actions_report.py
@@ -109,7 +109,7 @@ class IrActionsReport(models.Model):
             super(IrActionsReport, self)._prepare_html(html)
 
         # dump items...
-        #_logger.info(f'WKHTMLTOPDF: header({header.decode("utf-8")})')
+        _logger.info(f'WKHTMLTOPDF: header({header.decode("utf-8")})')
         #_logger.info(f'WKHTMLTOPDF: footer({footer.decode("utf-8")})')
 
         return bodies, res_ids, header, footer, specific_paperformat_args
