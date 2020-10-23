dfbe45c747c4a9e1d779a2fb93815e39ee2960ac
jbrauns <joshua_brauns@live.de>
2020-10-23 19:45:04 +0200

    Add generation of invoice lines

--- brauns_ml_sale/__manifest__.py
+++ brauns_ml_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Brauns-ML Sale",
-    "summray": "Allows to modify the default sales report template(s)",
+    "summary": "Allows to modify the default sales report template(s)",
     "verion": "13.0.1.0.0",
     "category": "Sales",
     "author": "Joshua Brauns",
